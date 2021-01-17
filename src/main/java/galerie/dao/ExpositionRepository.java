0877c98ec7cb00c195287e6b3652aefeec47c654
Gauthier <gauthierpous@bbox.fr>
2021-01-17 22:52:32 +0100

    Création de tous les tests : ils passent tous

--- src/main/java/galerie/dao/ExpositionRepository.java
+++ src/main/java/galerie/dao/ExpositionRepository.java
@@ -3,8 +3,16 @@ package galerie.dao;
 import galerie.entity.Exposition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface ExpositionRepository extends JpaRepository<Exposition, Integer>{
-
+    /**
+     * Calculer le chiffre d'affaires pour une exposition
+     * @param id la clé primaire de l'exposition
+     * @return le chiffre d'affaires de cette exposition
+     */
+    float chiffreAffairePour(Integer id);
+    //Initialisation de la somme à 0;
+    //Récupère toutes les transactions de l'exposition ayant pour id, l'id en paramètre
+    //Somme tous les prix de ventes des transactions
+    // "SELECT SUM(prix_vente) FROM Transaction, Exposition WHERE id = Exposition.id";
+    //Exposition ();
 }
